06dcfe248e47e29884ce93f89c30c51de0bf8711
Sourab Hossain <sourabhossain@github.com>
2020-07-17 17:39:29 +0600

    add woodCalculator function

--- assignment.js
+++ assignment.js
@@ -2,10 +2,9 @@
 function feetToMile(feet) {
     if (feet < 0) {
         return "Distance can't be negative.";
-    } else {
-        var mile = feet * 0.000189394; // here 1 foot = 0.000189394 mile 
     }
 
+    var mile = feet * 0.000189394; // here 1 foot = 0.000189394 mile 
     return mile.toFixed(10).toString() + " Mile";
 }
 
@@ -17,14 +16,20 @@ console.log(`Result: ${result}`);
 // Wood Calculator Function
 function woodCalculator(chair, table, bed) {
     if (chair < 0 || table < 0 || bed < 0) {
-        console.log("The number of furniture can't be negative.");
-    } else {
-        var woodForChair = chair * 1;
-        var woodForTable = table * 3;
-        var woodForBed = bed * 5;
-        var totalWood = woodForChair + woodForTable + woodForBed; 
+        return "The number of furniture can't be negative.";
     }
+
+    var woodForChair = chair * 1;
+    var woodForTable = table * 3;
+    var woodForBed = bed * 5;
+    var totalWood = woodForChair + woodForTable + woodForBed;
+    return totalWood.toString() + " Wood";
 }
+
+
+var result = woodCalculator(6, 1, 2);
+console.log(`Result: ${result}`);
+
 
 // brickCalculator
 function brickCalculator() {}
