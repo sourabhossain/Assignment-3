35039326027ab1182115e1f97449fcaafe080c9a
Sourab Hossain <sourabhossain@github.com>
2020-07-17 17:27:44 +0600

    update feetToMile

--- assignment.js
+++ assignment.js
@@ -1,19 +1,30 @@
-// Feet to Miles Conversion Program
+// Feet to Miles Conversion Function
 function feetToMile(feet) {
     if (feet < 0) {
-        console.log("Distance can't be negative.")
+        return "Distance can't be negative.";
     } else {
-        var mile = feet * 0.000189394;
+        var mile = feet * 0.000189394; // here 1 foot = 0.000189394 mile 
     }
 
-    return mile;
+    return mile.toFixed(10).toString() + " Mile";
 }
 
-var result = feetToMile(100);
-console.log(result.toFixed(10));
 
-// woodCalculator
-function woodCalculator() {}
+var result = feetToMile(100);
+console.log(`Result: ${result}`);
+
+
+// Wood Calculator Function
+function woodCalculator(chair, table, bed) {
+    if (chair < 0 || table < 0 || bed < 0) {
+        console.log("The number of furniture can't be negative.");
+    } else {
+        var woodForChair = chair * 1;
+        var woodForTable = table * 3;
+        var woodForBed = bed * 5;
+        var totalWood = woodForChair + woodForTable + woodForBed; 
+    }
+}
 
 // brickCalculator
 function brickCalculator() {}
