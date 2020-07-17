115dc2a66c983422a016778fc7ac2ba08da3e6bf
Sourab Hossain <sourabhossain@github.com>
2020-07-17 17:53:23 +0600

    add tiny friend function

--- assignment.js
+++ assignment.js
@@ -1,15 +1,17 @@
-// Feet to Miles Conversion Function
+// Feet To Miles Conversion Function
 function feetToMile(feet) {
     if (feet < 0) {
         return "Distance can't be negative.";
     }
 
     var mile = feet * 0.000189394; // here 1 foot = 0.000189394 mile 
+    
     return mile.toFixed(10).toString() + " Mile";
 }
 
 
 var result = feetToMile(100);
+
 console.log(`Result: ${result}`);
 
 
@@ -23,16 +25,41 @@ function woodCalculator(chair, table, bed) {
     var woodForTable = table * 3;
     var woodForBed = bed * 5;
     var totalWood = woodForChair + woodForTable + woodForBed;
+    
     return totalWood.toString() + " Wood";
 }
 
 
 var result = woodCalculator(6, 1, 2);
+
 console.log(`Result: ${result}`);
 
 
 // brickCalculator
 function brickCalculator() {}
 
-// tinyFriend
-function tinyFriend() {}
+
+// Find Smallest Friend Name From Friend Circle Function 
+function tinyFriend(friend) {
+    if (friend.length == 0) {
+        return "Friend list is empty.";
+    }
+
+    var smallestName = friend[0];
+    
+    for (var item = 0; item < friend.length; item++) {
+        var element = friend[item];
+
+        if (element.length < smallestName.length) {
+            smallestName = element;
+        }
+    }
+
+    return smallestName;
+}
+
+
+var friends = ["Sourab", "Pamel", "Ebrahim", "Shreerupa", "Avishek"];
+var result = tinyFriend(friends);
+
+console.log(`Result: ${result}`);
