8b1381d5561f978cff72cb1985895a25ca4c2095
Sourab Hossain <sourabhossain@github.com>
2020-07-17 11:02:06 +0600

    assignment init

---
+++ assignment.js
@@ -0,0 +1,11 @@
+// feetToMile
+function feetToMile() {}
+
+// woodCalculator
+function woodCalculator() {}
+
+// brickCalculator
+function brickCalculator() {}
+
+// tinyFriend
+function tinyFriend() {}
