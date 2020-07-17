3562938aff40f881ef558f2a41a3f2f5e6b4cea1
Sourab Hossain <sourabhossain@github.com>
2020-07-17 18:21:49 +0600

    add brickCalculator function

--- assignment.js
+++ assignment.js
@@ -35,9 +35,38 @@ var result = woodCalculator(6, 1, 2);
 console.log(`Result: ${result}`);
 
 
-// brickCalculator
-function brickCalculator() {}
+// Brick Calculation Function 
+function brickCalculator(buildingSize) {
+    if (buildingSize <= 0) {
+        return "Building size can't be negative or zero.";
+    }
 
+    var oneToTen = 15;
+    var elevenToTwenty = 12;
+    var twentyOneToAll = 10;
+    var totalBricks;
+
+    if (buildingSize <= 10) {
+        totalBricks = buildingSize * 1000;
+    } else if (buildingSize <= 20) {
+        oneToTen *= 10;
+        elevenToTwenty = buildingSize - 10;
+        buildingSize = oneToTen + (elevenToTwenty * 12);
+        totalBricks = buildingSize * 1000;
+    } else {
+        oneToTen *= 10;
+        elevenToTwenty *= 10;
+        twentyOneToAll = buildingSize - 20;
+        buildingSize = oneToTen + elevenToTwenty + (twentyOneToAll * 10);
+        totalBricks = buildingSize * 1000; 
+    }
+
+    return totalBricks + " Bricks";
+}
+
+var result = brickCalculator(100);
+
+console.log(`Result: ${result}`);
 
 // Find Smallest Friend Name From Friend Circle Function 
 function tinyFriend(friend) {
