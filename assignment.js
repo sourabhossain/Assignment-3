6b5aa33c785433bba9d95e24381d8c383b13d96d
Sourab Hossain <sourabhossain@github.com>
2020-07-17 17:14:44 +0600

    add feetToMile function

--- assignment.js
+++ assignment.js
@@ -1,5 +1,16 @@
-// feetToMile
-function feetToMile() {}
+// Feet to Miles Conversion Program
+function feetToMile(feet) {
+    if (feet < 0) {
+        console.log("Distance can't be negative.")
+    } else {
+        var mile = feet * 0.000189394;
+    }
+
+    return mile;
+}
+
+var result = feetToMile(100);
+console.log(result.toFixed(10));
 
 // woodCalculator
 function woodCalculator() {}
